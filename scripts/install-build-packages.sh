79dce0509d5a53a0c90bda4de638f50b4f49a04e
Charles Galambos <charles@reactai.com>
2024-07-26 07:22:23 +0100

    Install more packages.

--- scripts/install-build-packages.sh
+++ scripts/install-build-packages.sh
@@ -2,4 +2,4 @@
 
 # Install dependencies
 sudo apt-get update
-sudo apt-get install -y build-essential libopencv-dev libxtensor-dev libxtensor-blas-dev libspdlog-dev libfmt-dev
+sudo apt-get install -y build-essential libopencv-dev libxtensor-dev libopenblas-dev libxtensor-blas-dev liblapack-dev libspdlog-dev libfmt-dev
