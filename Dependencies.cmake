7ccc088479f67ef3b1bdd30d1f422b23b2171471
Charles Galambos <craftit@gmail.com>
2024-07-29 12:32:26 +0100

    Use native libs where possible.

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -5,11 +5,24 @@ include(cmake/CPM.cmake)
 # targets
 function(RAVL2_setup_dependencies)
 
+  # Try and use native packages if they're available
+
+  find_package(fmt QUIET)
+  find_package(spdlog QUIET)
+  find_package(xtensor QUIET)
+  find_package(xtensor-blas QUIET)
+  find_package(Catch2 3 QUIET)
+  find_package(CLI11 QUIET)
+  find_package(nlohmann_json QUIET)
+
   # For each dependency, see if it's
   # already been provided to us by a parent project
 
-  if(NOT TARGET fmtlib::fmtlib)
+  if(NOT TARGET fmt::fmt)
     cpmaddpackage("gh:fmtlib/fmt#9.1.0")
+  else()
+    message(STATUS "Found native fmt::fmt")
+    add_library(libfmt::libfmt ALIAS fmt::fmt)
   endif()
 
   if(NOT TARGET spdlog::spdlog)
@@ -22,14 +35,20 @@ function(RAVL2_setup_dependencies)
       "gabime/spdlog"
       OPTIONS
       "SPDLOG_FMT_EXTERNAL ON")
+  else ()
+    message(STATUS "Found native spdlog::spdlog")
   endif()
 
   if(NOT TARGET Catch2::Catch2WithMain)
     cpmaddpackage("gh:catchorg/Catch2@3.3.2")
+  else()
+    message(STATUS "Found native Catch2::Catch2WithMain")
   endif()
 
   if(NOT TARGET CLI11::CLI11)
     cpmaddpackage("gh:CLIUtils/CLI11@2.3.2")
+  else()
+    message(STATUS "Found native CLI11::CLI11")
   endif()
 
   if(NOT TARGET xtl)
@@ -43,6 +62,8 @@ function(RAVL2_setup_dependencies)
             GIT_TAG
             "0.7.7"
     )
+  else()
+    message(STATUS "Found native xtl")
   endif()
 
   if(NOT TARGET xtensor)
@@ -56,6 +77,8 @@ function(RAVL2_setup_dependencies)
             GIT_TAG
             "0.23.10"
             )
+  else()
+    message(STATUS "Found native xtensor")
   endif()
 
   if(NOT TARGET xtensor-blas)
@@ -69,16 +92,18 @@ function(RAVL2_setup_dependencies)
             GIT_TAG
             "0.17.1"
             )
+  else ()
+    message(STATUS "Found native xtensor-blas")
   endif()
 
-#  if(NOT TARGET OpenCV::OpenCV)
-#    cpmaddpackage("gh:opencv/opencv@4.5.3")
+  if(NOT TARGET nlohmann_json::nlohmann_json)
+    CPMAddPackage("gh:nlohmann/json@3.10.5")
+  else()
+    message(STATUS "Found native nlohmann_json::nlohmann_json")
+  endif()
+
+#  if(NOT TARGET tools::tools)
+#    cpmaddpackage("gh:lefticus/tools#update_build_system")
 #  endif()
-
-  #CPMAddPackage("gh:nlohmann/json@3.10.5")
-
-  if(NOT TARGET tools::tools)
-    cpmaddpackage("gh:lefticus/tools#update_build_system")
-  endif()
 
 endfunction()
