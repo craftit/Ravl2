085cd56bd0150fb496a0d46622e414a53891e47f
Charles Galambos <craftit@gmail.com>
2025-01-27 13:47:40 +0000

    Cube works on Apple.

--- src/Ravl2/OpenGL/GLWindow.cc
+++ src/Ravl2/OpenGL/GLWindow.cc
@@ -90,8 +90,8 @@ namespace Ravl2
     mGlsl_version = "#version 150";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
 #else
     // GL 3.0 + GLSL 130
     //mGlsl_version = "#version 150";
