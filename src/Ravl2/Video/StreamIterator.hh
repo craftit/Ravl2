7dbf3b64a05848ec48f75ad581e63751f3b18ca1
Charles Galambos <craftit@gmail.com>
2025-11-10 11:16:51 +0000

    Avoid potential nullptr deref.

--- src/Ravl2/Video/StreamIterator.hh
+++ src/Ravl2/Video/StreamIterator.hh
@@ -193,7 +193,7 @@ namespace Ravl2::Video
     {
       assert(m_iterator->currentFrame());
       auto ptr = std::dynamic_pointer_cast<VideoFrame<ImageTypeT>>(m_iterator->currentFrame());
-      assert(ptr);
+      RavlAlwaysAssert(ptr != nullptr);
       return *ptr;
     }
 
