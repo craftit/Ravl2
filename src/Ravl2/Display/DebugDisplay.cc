2624c9c16802bf458a87f3ce2f99ff8e834dc797
Charles Galambos <craftit@gmail.com>
2025-11-10 09:21:22 +0000

    GCC fixes.

--- src/Ravl2/Display/DebugDisplay.cc
+++ src/Ravl2/Display/DebugDisplay.cc
@@ -177,7 +177,9 @@ namespace {
 // Important: include SDL backends BEFORE the bgfx-wrapped imgui.hh since it may define IMGUI_DISABLE
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wold-style-cast"
+#if defined(__clang__)
 #pragma GCC diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
+#endif
 #include <imgui.h>
 #include <backends/imgui_impl_sdl2.h>
 #include <backends/imgui_impl_sdlrenderer2.h>
